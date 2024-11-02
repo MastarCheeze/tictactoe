b2a317ac5b7b53f2e322d01fd2aef4c41ba2bf48
MastarCheeze <mon10180218@gmail.com>
2024-11-02 12:27:44 +0800

    export functions inline with c code

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
-SRC = src/c/tictactoe.c
+SRC_C = src/c/tictactoe.c
+SRC_JS = src/js/main.js
 TARGET = build/tictactoe
 EMCC = emcc
 
@@ -7,7 +8,7 @@ all: $(TARGET).wasm
 $(TARGET).wasm: $(SRC)
 	$(EMCC) $(SRC) -o $(TARGET).js \
 		-sWASM=1 \
-		-sEXPORTED_FUNCTIONS='["_noughts","_crosses","_currentPlayer","_checkWin","_checkTie","_getPlayer"]' \
+		-sEXPORTED_RUNTIME_METHODS=getValue,setValue \
 		-sMODULARIZE=1 \
 		-sEXPORT_ES6=1 \
 		-sENVIRONMENT=web
--- src/c/tictactoe.c
+++ src/c/tictactoe.c
@@ -1,8 +1,9 @@
 #include <stdbool.h>
+#include <emscripten.h>
 
-int noughts = 0;
-int crosses = 0;
-int currentPlayer = 0; // 0 for noughts, 1 for crosses
+int EMSCRIPTEN_KEEPALIVE noughts = 0;
+int EMSCRIPTEN_KEEPALIVE crosses = 0;
+int EMSCRIPTEN_KEEPALIVE currentPlayer = 0; // 0 for noughts, 1 for crosses
 
 static int allMask = (1 << 9) - 1;
 static int winMasks[] = {
@@ -11,7 +12,7 @@ static int winMasks[] = {
     0b100010001, 0b001010100,              // diagonal
 };
 
-bool checkWin(int mask) {
+bool EMSCRIPTEN_KEEPALIVE checkWin(int mask) {
   for (int i = 0; i < 8; i++) {
     int winMask = winMasks[i];
     if ((mask & winMask) == winMask)
@@ -20,16 +21,20 @@ bool checkWin(int mask) {
   return false;
 }
 
-bool checkTie(int mask1, int mask2) {
-  if ((mask1 & mask2 & allMask) == allMask)
+bool EMSCRIPTEN_KEEPALIVE checkTie(int mask1, int mask2) {
+  if ((mask1 | mask2 & allMask) == allMask)
     return true;
   else
     return false;
 }
 
-int getPlayer(int player) {
+int EMSCRIPTEN_KEEPALIVE getPlayer(int player) {
   if (currentPlayer == 0)
     return noughts;
   else
     return crosses;
+}
+
+int EMSCRIPTEN_KEEPALIVE test() {
+  return noughts;
 }
