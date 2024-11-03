34dd8ec232e04d3c1af19989a2b98bae6f6d79bb
MastarCheeze <mon10180218@gmail.com>
2024-11-03 14:05:15 +0800

    negamax

--- src/c/tictactoe.c
+++ src/c/tictactoe.c
@@ -22,3 +22,48 @@ int EMSCRIPTEN_KEEPALIVE checkTie(int mask1, int mask2) {
   else
     return 0;
 }
+
+int countBits(int value) {
+  int count = 0;
+  while (value) {
+    value &= value - 1;
+    count++;
+  }
+  return count;
+}
+
+// extremely simple negamax search algorithm
+// no alpha beta, no hash tables (im lazy)
+int negamax(int playerMask, int opponentMask) {
+  if (checkWin(opponentMask))
+    // play with food, prefer to win in most moves possible
+    return -(100 + countBits(playerMask & opponentMask));
+  else if (checkTie(playerMask, opponentMask)) {
+    return 0;
+  }
+
+  int value = -420;
+  for (int i = 1; i < (1 << 9); i <<= 1) {
+    if (!((playerMask | opponentMask) & i)) {
+      int heuristic = -negamax(opponentMask, playerMask | i);
+      if (heuristic > value)
+        value = heuristic;
+    }
+  }
+  return value;
+}
+
+int EMSCRIPTEN_KEEPALIVE calcBestMove(int playerMask, int opponentMask) {
+  int bestMove = -1;
+  int bestMoveHeuristic = -420;
+  for (int i = 1; i < (1 << 9); i <<= 1) {
+    if (!((playerMask | opponentMask) & i)) {
+      int heuristic = -negamax(opponentMask, playerMask | i);
+      if (heuristic > bestMoveHeuristic) {
+        bestMove = i;
+        bestMoveHeuristic = heuristic;
+      }
+    }
+  }
+  return bestMove;
+}
