25a71ff1f1612433306633545022c654fc4068ba
MastarCheeze <mon10180218@gmail.com>
2024-11-03 09:57:39 +0800

    build game logic

--- src/c/tictactoe.c
+++ src/c/tictactoe.c
@@ -1,9 +1,4 @@
-#include <stdbool.h>
 #include <emscripten.h>
-
-int EMSCRIPTEN_KEEPALIVE noughts = 0;
-int EMSCRIPTEN_KEEPALIVE crosses = 0;
-int EMSCRIPTEN_KEEPALIVE currentPlayer = 0; // 0 for noughts, 1 for crosses
 
 static int allMask = (1 << 9) - 1;
 static int winMasks[] = {
@@ -12,29 +7,18 @@ static int winMasks[] = {
     0b100010001, 0b001010100,              // diagonal
 };
 
-bool EMSCRIPTEN_KEEPALIVE checkWin(int mask) {
+int EMSCRIPTEN_KEEPALIVE checkWin(int mask) {
   for (int i = 0; i < 8; i++) {
     int winMask = winMasks[i];
     if ((mask & winMask) == winMask)
-      return true;
+      return winMask;
   }
-  return false;
+  return 0;
 }
 
-bool EMSCRIPTEN_KEEPALIVE checkTie(int mask1, int mask2) {
+int EMSCRIPTEN_KEEPALIVE checkTie(int mask1, int mask2) {
   if ((mask1 | mask2 & allMask) == allMask)
-    return true;
+    return 1;
   else
-    return false;
-}
-
-int EMSCRIPTEN_KEEPALIVE getPlayer(int player) {
-  if (currentPlayer == 0)
-    return noughts;
-  else
-    return crosses;
-}
-
-int EMSCRIPTEN_KEEPALIVE test() {
-  return noughts;
+    return 0;
 }
--- src/web/main.js
+++ src/web/main.js
@@ -1,5 +1,88 @@
 import createModule from "../../build/tictactoe.js";
+import "./style.css";
 
+// Initialise WASM module
 const module = await createModule();
-console.log(module);
-window.module = module;
+const players = [0, 0];
+let curPlayer = 0;
+const checkWin = module._checkWin;
+const checkTie = module._checkTie;
+
+// Get DOM elements
+const status = document.querySelector("#status");
+const reset = document.querySelector("#reset");
+const cells = [];
+for (let i = 0; i < 9; i++) {
+  cells[i] = document.querySelector(`#cell-${i + 1}`);
+}
+
+function updateGrid() {
+  for (let i = 0; i < 9; i++) {
+    if (players[0] & (1 << i)) {
+      cells[i].innerText = getSymbol(0);
+    } else if (players[1] & (1 << i)) {
+      cells[i].innerText = getSymbol(1);
+    } else {
+      cells[i].innerText = "";
+    }
+  }
+}
+
+function getSymbol(player) {
+  if (player == 0) {
+    return "O";
+  } else {
+    return "X";
+  }
+}
+
+/* Events */
+for (let i = 0; i < 9; i++) {
+  cells[i].addEventListener("click", () => {
+    if (cells[i].classList.contains("disable")) {
+      return;
+    }
+
+    // play move
+    players[curPlayer] |= 1 << i;
+
+    // check game over
+    const winMask = checkWin(players[curPlayer]);
+    if (winMask) {
+      status.innerText = `${getSymbol(curPlayer)} wins!`;
+      for (let i = 0; i < 9; i++) {
+        cells[i].classList.add("disable");
+        if (winMask & (1 << i)) {
+          cells[i].classList.add("highlight");
+        }
+      }
+    } else if (checkTie(...players)) {
+      status.innerText = `Tie!`;
+      for (let i = 0; i < 9; i++) {
+        cells[i].classList.add("disable");
+      }
+    } else {
+      cells[i].classList.add("disable");
+      curPlayer ^= 1;
+      status.innerText = `${getSymbol(curPlayer)}'s turn`;
+    }
+
+    updateGrid();
+  });
+}
+
+reset.addEventListener("click", () => {
+  players[0] = 0;
+  players[1] = 0;
+  curPlayer = 0;
+  for (let i = 0; i < 9; i++) {
+    cells[i].classList.remove("disable", "highlight");
+  }
+
+  status.innerText = `${getSymbol(curPlayer)}'s turn`;
+  updateGrid();
+});
+
+// initialise
+status.innerText = `${getSymbol(curPlayer)}'s turn`;
+updateGrid();
